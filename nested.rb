75b4248e338935e65bf6219a8c18c5c6ecb9763a
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-14 21:15:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  
+  return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     str = programmer_hash[:dennis_ritchie][:languages]
+     puts str
+     
 end
 
 def adding_matz
