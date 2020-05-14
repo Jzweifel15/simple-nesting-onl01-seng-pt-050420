be4bd1485c44313a149d5e814e8ee381bd9f8286
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-14 21:22:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,8 +61,8 @@ def dennis_ritchies_language
         }
      }
      
-     str = programmer_hash[:dennis_ritchie][:languages].to_s
-     return str
+     return programmer_hash[:dennis_ritchie][:languages][0]
+     
      
 end
 
